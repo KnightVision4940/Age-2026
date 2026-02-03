54b11fe68c39dac423290d85b8188ad892343e9e
matthewmacmillan2010 <matthewmacmillan2010@gmail.com>
2026-02-02 19:06:37 -0500

    Initialized shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import frc.robot.Constants;
+
+
+public class Shooter extends SubsystemBase {
+
+  SparkMax shooterMotor;
+
+  public void Shooter(){
+    shooterMotor = new SparkMax(Constants.MotorIDs.shooterMotor, MotorType.kBrushless);
+  }
+
+  public void shooterShoot(double speed){
+    shooterMotor.set(speed);
+  }
+}
