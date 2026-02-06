a14580d0256860ad0dd53791508305bc28f3c10e
matthewmacmillan2010 <matthewmacmillan2010@gmail.com>
2026-02-05 20:02:24 -0500

    closed loop controller

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,6 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import frc.robot.Constants;
 
@@ -13,13 +16,21 @@ import frc.robot.Constants;
 public class Shooter extends SubsystemBase {
 
   SparkMax shooterMotor;
+  SparkClosedLoopController m_Controller;
 
   public void Shooter(){
     shooterMotor = new SparkMax(Constants.MotorIDs.shooterMotor, MotorType.kBrushless);
+    m_Controller = shooterMotor.getClosedLoopController();
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.closedLoop
+      .p(1)
+      .i(0)
+      .d(0)
+      .outputRange(1, -1);
   }
 
   public void shooterShoot(double speed){
-    shooterMotor.set(speed);
+    m_Controller.setSetpoint(speed, ControlType.kVelocity);
   }
 
   public void Stop(){
