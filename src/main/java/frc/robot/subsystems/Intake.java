20662730dd68670e8725f109b5a15c62ee0a8316
KevinLovesCurry <kevinduan2051@gmail.com>
2026-02-10 18:44:35 -0500

    ClosedLoopIntakeWork

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,26 +10,38 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  public SparkMax m_intake; 
+  public SparkMax m_roller;
+  public SparkMax m_pivot; 
+  public SparkClosedLoopController m_pivotcontroller;
   
   /** Creates a new Intake. */
   public Intake() {
-    m_intake = new SparkMax(Constants.Motors.Intake.kId, MotorType.kBrushless);
-
+    m_roller = new SparkMax(Constants.Motors.Intake.kId, MotorType.kBrushless);
+    m_pivot = new SparkMax(Constants.Motors.Intake.kId, MotorType.kBrushless);
     SparkMaxConfig config = new SparkMaxConfig();
     config
         .smartCurrentLimit(Constants.Motors.Intake.kCurrentLimit)
         .idleMode(IdleMode.kBrake);
+    
+    
+      m_pivotcontroller = m_pivot.getClosedLoopController();
+    config.closedLoop
+      .p(1)
+      .i(0)
+      .d(0)
+      .outputRange(-1, 1);
 
     // Persist parameters to retain configuration in the event of a power cycle
-    m_intake.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
+    m_roller.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_pivot.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
   }
 
@@ -43,16 +55,28 @@ public class Intake extends SubsystemBase {
   }
 
   public void spin(double speed) {
-    m_intake.set(speed);
+    m_roller.set(speed);
+    
+  }
+
+  public void pivotIntake(double position){
+    m_pivotcontroller.setSetpoint(position, ControlType.kPosition);
+    
   }
 
   public void stop() {
-    m_intake.set(0);
+    m_roller.set(0);
   }
+  public void pivotStop() {
+    m_pivot.set(0);
+  }
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  
 }
 
