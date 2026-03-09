678729a895ae25c776f629816f282c1667e99135
matthewmacmillan2010 <matthewmacmillan2010@gmail.com>
2026-03-09 19:17:43 -0400

    conditionals for stopping

--- src/main/java/frc/robot/commands/ManualClimbControl.java
+++ src/main/java/frc/robot/commands/ManualClimbControl.java
@@ -5,12 +5,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.Climber;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ManualClimbControl extends Command {
   double speed;
   Climber climber;
+
+
   public ManualClimbControl(Climber climber, double speed) {
     this.climber = climber;
     this.speed = speed;
@@ -37,6 +40,21 @@ public class ManualClimbControl extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+
+    boolean goingUp = speed > 0;
+    boolean goingDown = speed < 0;
+
+    boolean atMaxHeight = climber.getPosition() >= Constants.ClimbPositions.top;
+    boolean atMinHeight = climber.getPosition() <= Constants.ClimbPositions.bottom;
+
+    if ((climber.isLocked() || atMaxHeight) && goingUp) {
+      climber.stop();
+    }
+
+    if (atMinHeight && goingDown) {
+      climber.stop();
+    }
+
     return false;
   }
 }
