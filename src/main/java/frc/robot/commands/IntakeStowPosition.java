2cc2b634baa0dc8f47aa3d018395533a70646eed
matthewmacmillan2010 <matthewmacmillan2010@gmail.com>
2026-03-27 08:19:17 -0400

    stow position get pivot position

--- src/main/java/frc/robot/commands/IntakeStowPosition.java
+++ src/main/java/frc/robot/commands/IntakeStowPosition.java
@@ -38,6 +38,6 @@ public class IntakeStowPosition extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return this.intake.getPivotPosition() <= 0.0;
   }
 }
