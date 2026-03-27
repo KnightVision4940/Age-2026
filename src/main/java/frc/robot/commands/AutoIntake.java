911583bfdd0e28a61647eda04f1ae95a318982a3
matthewmacmillan2010 <matthewmacmillan2010@gmail.com>
2026-03-26 20:56:09 -0400

    updated encoder offsets

--- src/main/java/frc/robot/commands/AutoIntake.java
+++ src/main/java/frc/robot/commands/AutoIntake.java
@@ -28,7 +28,7 @@ public class AutoIntake extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intake.spin(1);
+    intake.spin(-1);
   }
 
   // Called once the command ends or is interrupted.
