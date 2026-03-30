48cc60b957dd68def5f30319f0d4cb829c60e1c3
matthewmacmillan2010 <matthewmacmillan2010@gmail.com>
2026-03-30 19:41:37 -0400

    changed outtake feeder speed

--- src/main/java/frc/robot/commands/OuttakeFeeder.java
+++ src/main/java/frc/robot/commands/OuttakeFeeder.java
@@ -23,7 +23,7 @@ public class OuttakeFeeder extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    this.feeder.spin(-0.1);
+    this.feeder.spin(-0.3);
   }
 
   // Called once the command ends or is interrupted.
